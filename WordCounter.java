82ddb6c171d38b039ee7532e8d30b189017f95aa
edward <145471329+Edward-Bae-00@users.noreply.github.com>
2024-10-18 10:45:06 -0400

    Initial Commit

--- WordCounter.java
+++ WordCounter.java
@@ -1,14 +1,2 @@
 public class WordCounter {
-    public int processText(StringBuffer text, String stopword){
-        return 0;
-    }
-
-    public StringBuffer processFile(String path){
-        StringBuffer ans = new StringBuffer();
-        return ans;
-    }
-
-    public void main(){
-        
-    }
 }
