d5c91051d84e6077f19ed7e0f423673de0e1fb9c
edward <145471329+Edward-Bae-00@users.noreply.github.com>
2024-10-21 14:27:00 -0400

    commit during lab

--- WordCounter.java
+++ WordCounter.java
@@ -1,2 +1,22 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WordCounter {
+    public int processText(StringBuffer text, String stopword){
+        Pattern regex = Pattern.compile("your regular expression here");
+        Matcher regexMatcher = regex.matcher(text);
+        while (regexMatcher.find()) {
+            System.out.println("I just found the word: " + regexMatcher.group());
+        } 
+        return 0;
+    }
+
+    public StringBuffer processFile(String path){
+        StringBuffer ans = new StringBuffer();
+        return ans;
+    }
+
+    public void main(){
+        
+    }
 }
